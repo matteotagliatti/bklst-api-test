a12a50a3d0b0d0b263d63b36789f199a40c33605
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-13 19:14:08 +0200

    add read and reading endpoints

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,40 @@ import getUser from "../utils/login.js";
 router.get("/", async function (req, res, next) {
   try {
     const user_id = await getUser();
-    const { data: books, error2 } = await supabase
+    const { data: books, error } = await supabase
       .from("books")
       .select()
       .eq("owner", user_id);
+
+    res.json(books);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/read", async function (req, res, next) {
+  try {
+    const user_id = await getUser();
+    const { data: books, error } = await supabase
+      .from("books")
+      .select()
+      .eq("owner", user_id)
+      .eq("status", "read");
+
+    res.json(books);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/reading", async function (req, res, next) {
+  try {
+    const user_id = await getUser();
+    const { data: books, error } = await supabase
+      .from("books")
+      .select()
+      .eq("owner", user_id)
+      .eq("status", "reading");
 
     res.json(books);
   } catch (error) {

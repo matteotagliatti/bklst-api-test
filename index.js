8e5e6a106937d3652e1977970a66a8200ed15c32
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-13 15:32:01 +0200

    express init

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import indexRouter from "./routes/index.js";
+const port = 3000;
+
+const app = express();
+
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

4fcb24d52f2a454ba6123bee85bdc0f78b3c6f4f
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-13 18:29:02 +0200

    supabase fetch my books

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import indexRouter from "./routes/index.js";
+import loginRouter from "./routes/login.js";
 const port = 3000;
 
 const app = express();
 
 app.use("/", indexRouter);
+app.use("/login", loginRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routes/login.js
+++ routes/login.js
@@ -2,23 +2,13 @@ import express from "express";
 const router = express.Router();
 import supabase from "../config.js";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
 router.get("/", async function (req, res, next) {
   try {
     const { data, error } = await supabase.auth.signInWithPassword({
-      email: process.env.USER_EMAIL,
-      password: process.env.USER_PASSWORD,
+      email: "matteotagliatti@gmail.com",
+      password: "Bradipo1994",
     });
-    const user_id = data.session.user.id;
-
-    const { data: books, error2 } = await supabase
-      .from("books")
-      .select()
-      .eq("owner", user_id);
-
-    res.json(books);
+    res.json(data);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

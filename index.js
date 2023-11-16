c636ca0f592f550d066d40cfb10794af01954d23
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-16 14:26:16 +0100

    Add authentication routes and Supabase integration

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ const express = require("express");
 const dotenv = require("dotenv");
 const port = 3000;
 
+dotenv.config();
+
 const { createClient } = require("./lib/supabase");
 
 const app = express();
 
-app.get("/books", async function (req, res, next) {
+app.get("/auth/confirm", async function (req, res) {
+  const token_hash = req.query.token_hash;
+  const type = req.query.type;
+  const next = req.query.next ?? "/";
+
+  if (token_hash && type) {
+    const supabase = createClient({ req, res });
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
+    if (!error) {
+      res.redirect(303, `/${next.slice(1)}`);
+    }
+  }
+
+  throw new Error("Invalid token");
+});
+
+app.get("/auth/signin", async function (req, res) {
   const supabase = createClient({ req, res });
 
-  const { data } = await supabase.from("books").select("*");
-  res.json(data);
+  console.log(supabase);
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email: req.query.email,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  res.redirect(303, `/auth/confirm?type=signin&next=${req.query.next}`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
